835263e371734afee783edfc38a0defdca920a7f
kaiya gunsauls <kaiyagunsauls@gmail.com>
2021-09-28 21:21:32 -0700

    some progress, still not totally functioning but on the right path

--- script.js
+++ script.js
@@ -1,49 +1,28 @@
 
-
-// })
-/* TODO: add timer that stops quiz at 0
-TODO: best scores page in lcal storage
-TODO: add css to make pages dissappear when answer chosen
-TODO: add reset button maybe*/
-
-
-
-//press start button
-//first question card appears
-//timer starts
-//select an answer
-//click next question button
-
-//title card vars
-var titleContainer = document.querySelector(".card");
-var startButton = document.body.querySelector(".card__start");
-
-//question 1 vars
-// var firstQuestion = document.querySelector("questions");
-var questionCard = document.querySelector(".questions");
-var nextQuestion = document.querySelector(".question__button");
-
+var answerButtonOne = document.body.querySelector(".answer-button-one");
+var answerButtonTwo = document.body.querySelector(".answer-button-two");
+var answerButtonThree = document.body.querySelector(".answer-button-three");
+var answerButtonFour = document.body.querySelector(".answer-button-four");
 var timer = document.body.querySelector(".nav__timer");
 var secondsLeft = 120;
+var startButton = document.querySelector(".start-button");
+var scoreCount = document.querySelector(".scoreboard");
+var score = 0;
 
-
-//other questions will be used later lol
-// var secondQuestion = document.querySelector("#second-question");
-// var thirdQuestion = document.querySelector("#third-question");
-// var fourthQuestion = document.querySelector("#fourth-question");
-
-var answers = [];
-
+scoreCount.textContent = "Score: " + score;
 
 timer.textContent = "Time: " + secondsLeft;
 
-// console.log(startButton);
-startButton.addEventListener("click", startQuiz)
+startButton.addEventListener("click", startTimer);
 
-document.getElementById("questions").style.display = "none";
+document.querySelector(".first").style.display = "none";
+document.querySelector(".second").style.display = "none";
+document.querySelector(".third").style.display = "none";
+document.querySelector(".fourth").style.display = "none";
 
-function startQuiz() {
-
+function startTimer() {
+    document.querySelector(".first").style.display = "block";
+    document.querySelector(".title-page").style.display = "none";
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timer.textContent = "Time: " + secondsLeft;
@@ -53,62 +32,20 @@ function startQuiz() {
             
         }
     }, 1000);
-
-    showQuestion();
-
+      
 }
 
-function showQuestion(){
-
-    console.log("whats up");
-    
-    document.getElementById("questions").style.display = "block";
-
-    var selectList = document.createElement("select");
-    selectList.id = "questionOne";
-    questionCard.appendChild(selectList);
-
-    for (var i = 0; i < questions.length; i++) {
-        var option = document.createElement("option");
-        option.value = questions[0] ['choices'][i];
-        option.text = questions[0]['choices'][i];
-        selectList.appendChild(option);
-    }
-    
-    
-}
-
-// nextQuestion.addEventListener("click", answerQuestion())
-
-// function answerQuestion() {
-    // var correct = document.getElementById("question-one-correct")
-    
-    
-
-var questions = [
-  {
-    question: "what's the capital of sweden?",
-    choices: ["texas", "blank", "random", "italy"],
-    answer: "blank",
-  },
-  {
-    question: "what's the capital of germany?",
-    choices: ["texas", "blank", "random", "italy"],
-    answer: "blank",
-  },
-  {
-    question: "what's the capital of denmark?",
-    choices: ["texas", "blank", "random", "italy"],
-    answer: "blank",
-  },
-];
-
-
-nextQuestion.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    console.log(questions[0]['answer']);
-
+document.addEventListener('click', e => {
+  if (e.target.matches('one')) {
+    console.log("hiiii")
+  }
 });
 
+// function clickAnswerOne() {
+//     document.querySelector(".first").style.display = "none";
+//     document.querySelector(".second").style.display = "block";
 
+//     if(document.getElementsByClassName("answer-button-one").value == "correct") {
+//       console.log("cool");
+//     };
+//   };
